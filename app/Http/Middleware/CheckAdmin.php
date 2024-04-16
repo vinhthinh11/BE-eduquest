d88f147bce17036ba4a0d4cdbcd66ed68c27f88b
vinhthinh11 <vinhthinh1993@gmail.com>
2024-04-16 12:53:04 +0700

    modified check when user are not authorize

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -10,14 +10,9 @@ class CheckAdmin
 {
     public function handle(Request $request, Closure $next)
     {
-
         $check = JWTAuth::parseToken()->authenticate();
-        if ($check->permission == 1) {
+        if (!$check) return response()->json(['message' => 'You are unauthorize for this route'],403);
             $request->id = $check->admin_id;
             return $next($request);
-        } else {
-            return response()->json(['message' => 'You are unauthorize for this route'],403);
-        }
-
     }
 }
--- app/Http/Middleware/CheckHeadSubject.php
+++ app/Http/Middleware/CheckHeadSubject.php
@@ -11,13 +11,9 @@ class CheckHeadSubject
     public function handle(Request $request, Closure $next)
     {
 
-        $check = JWTAuth::parseToken()->authenticate();
-        if ($check->permission == 4) {
+       $check = JWTAuth::parseToken()->authenticate();
+        if (!$check) return response()->json(['message' => 'You are unauthorize for this route'],403);
             $request->id = $check->subject_head_id;
             return $next($request);
-        } else {
-            return response()->json(['message' => 'You are unauthorize for this route'],403);
-        }
-
     }
 }
